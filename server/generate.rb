cf06d4a6b608b239c38b8eedc12cbeddc6074d74
Michel Grootjans <michel.grootjans@protime.eu>
2016-08-28 11:49:39 +0200

    added a simple generator

---
+++ server/generate.rb
@@ -0,0 +1,33 @@
+require 'json'
+require 'securerandom'
+require 'date'
+
+def generate_game
+  data = {
+    event: {
+
+    }
+    "game_created",
+    message_id: SecureRandom.uuid,
+    timestamp: DateTime.now,
+    payload: {
+      game: {
+        id: SecureRandom.uuid,
+        title: 'Game of Thrones quiz'
+      }
+    }
+  }
+end
+
+stream_name = ARGV.first || 'stream1'
+file_name = "streams/#{stream_name}.json"
+
+puts "Generating #{file_name}"
+
+data = []
+data << generate_game
+
+open(file_name, 'w') do |file|
+  file.truncate(0)
+  file.puts data.to_json
+end
