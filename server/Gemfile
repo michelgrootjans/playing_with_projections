2a03d79fe7c0144163bf7774f601ba10f7958b12
Michel Grootjans <michel.grootjans@protime.eu>
2016-08-28 14:39:27 +0200

    fake some events

--- server/Gemfile
+++ server/Gemfile
@@ -2,3 +2,6 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'faker'
+gem 'pry'
+gem 'pry-nav'
+gem 'pry-stack_explorer'
--- server/history_generator.rb
+++ server/history_generator.rb
@@ -1,4 +1,40 @@
 require 'faker'
+require 'pry'
+
+class GameOfThronesTheme
+  def title
+    Faker::Book.title
+  end
+end
+
+class Game
+  attr_reader :id, :title, :created_at, :published_at
+  def initialize
+    @id = SecureRandom.uuid
+    @created_at = Faker::Time.between(DateTime.now - 365, DateTime.now)
+    @published_at = created_at + Random.new.rand(60*60*24)
+    @theme = GameOfThronesTheme.new
+    @title = @theme.title
+  end
+
+  def new_quiz
+    Quiz.new(self)
+  end
+end
+
+class Quiz
+  attr_reader :id, :game_id, :started_at, :ended_at
+  def initialize(game)
+    @id = SecureRandom.uuid
+    @game_id = game.id
+    @started_at = Faker::Time.between(game.published_at, DateTime.now)
+    @ended_at = @started_at + 10 + Random.new.rand(60*60)
+  end
+
+  def ended?
+    ended_at < Time.now
+  end
+end
 
 class HistoryGenerator
   def generate_history(number_of_games)
@@ -8,39 +44,47 @@ class HistoryGenerator
   private
 
   def generate_game
-    game_id = SecureRandom.uuid
-    game_created = Faker::Time.between(DateTime.now - 365, DateTime.now)
+    game = Game.new
     events = []
-    events << game_created(game_id, game_created)
+    events << game_created(game)
+    events << game_published(game)
     (2..Random.new.rand(20)).each do
-      quiz_id = SecureRandom.uuid
-      quiz_start = Faker::Time.between(game_created, DateTime.now)
-      quiz_end = Faker::Time.between(quiz_start, quiz_start + (1.0/24))
-      events << quiz_started(game_id, quiz_id, quiz_start)
-      events << quiz_ended(game_id, quiz_id, quiz_end) if quiz_end < Time.now
+      quiz = game.new_quiz
+      events << quiz_started(quiz)
+      events << quiz_ended(quiz) if quiz.ended?
     end
     events.flatten.compact
 
   end
 
-  def game_created(game_id, timestamp)
+  def game_created(game)
     {
-        event: event_created('game_created', timestamp),
-        payload: {game: {id: game_id}}
+        event: event_created('game_created', game.created_at),
+        payload: {game: {
+            id: game.id,
+            title: game.title
+        }}
     }
   end
 
-  def quiz_started(game_id, quiz_id, timestamp)
+  def game_published(game)
     {
-        event: event_created('quiz_started', timestamp),
-        payload: {game_id: game_id, quiz_id: quiz_id}
+        event: event_created('game_published', game.published_at),
+        payload: {game_id: game.id}
     }
   end
 
-  def quiz_ended(game_id, quiz_id, timestamp)
+  def quiz_started(quiz)
     {
-        event: event_created('quiz_ended', timestamp),
-        payload: {game_id: game_id, quiz_id: quiz_id}
+        event: event_created('quiz_started', quiz.started_at),
+        payload: {game_id: quiz.game_id, quiz_id: quiz.id}
+    }
+  end
+
+  def quiz_ended(quiz)
+    {
+        event: event_created('quiz_ended', quiz.ended_at),
+        payload: {game_id: quiz.game_id, quiz_id: quiz.id}
     }
   end
 
