bba2c2a96e80daaeca0b7798285711eed0358025
Thomas Coopman <thomas.coopman@gmail.com>
2016-09-07 22:19:08 +0200

    move clients out of clients structure

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const https = require("https");
+
+function fetchStream(stream) {
+    const options = {
+        hostname: 'playing-with-projections.herokuapp.com',
+        path: `/stream/${stream}`,
+        method: 'GET'
+    };
+
+    return new Promise((resolve, reject) => {
+        let data = '';
+        const req = https.request(options, (res) => {
+            res.setEncoding('utf8');
+            res.on('data', (chunk) => data += chunk);
+            res.on('end', () => resolve(JSON.parse(data)))
+        });
+
+        req.on('error', e => reject(e));
+        req.end();
+    });
+}
+
+function registeredPlayersProjection(events) {
+    return events.reduce((acc, {type, payload}) => {
+        switch (type) {
+            case 'PlayerHasRegistered': {
+                acc[payload.player_id] = {first_name: payload.first_name, last_name: payload.last_name}
+                return acc;
+            }
+            default: return acc
+        }
+    }, {});
+}
+
+fetchStream(0)
+    .then(events => console.log(registeredPlayersProjection(events)))
+    .catch(error => console.log(error));
