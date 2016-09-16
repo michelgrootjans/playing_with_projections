d6f9b729a7ebc7a002ed66fb503e4fdd4a47388a
Clément Bouillier <clement.bouillier@devcrafting.com>
2016-09-16 20:25:16 +0200

    Add all types for stream 0

--- fsharp/client.fsx
+++ fsharp/client.fsx
@@ -11,21 +11,28 @@ type Event<'T> = {
 }
 
 type PlayerHasRegistered = JsonProvider<"""{"player_id":1,"last_name":"Coopman","first_name":"Thomas"}""">
+type QuizWasCreated = JsonProvider<"""{"quiz_title":"Cities of Europe","quiz_id":1,"owner_id":1}""">
+type QuestionAddedToQuiz = JsonProvider<"""{"quiz_id":1,"question_id":1,"question":"What is the capital of France?","answer":"Paris"}""">
+type QuizWasPublished = JsonProvider<"""{"quiz_id":1}""">
 
 type Events = 
     | PlayerHasRegistered of PlayerHasRegistered.Root
-    | UnknownEvent
+    | QuizWasCreated of QuizWasCreated.Root
+    | QuestionAddedToQuiz of QuestionAddedToQuiz.Root
+    | QuizWasPublished of QuizWasPublished.Root
 
-let parsePayload (payload:JsonValue) = function
-    | "PlayerHasRegistered" -> 
-        PlayerHasRegistered (PlayerHasRegistered.Parse(payload.ToString()))
-    | _ -> UnknownEvent
+let parsePayload payload = function
+    | "PlayerHasRegistered" -> PlayerHasRegistered (PlayerHasRegistered.Parse(payload))
+    | "QuizWasCreated" -> QuizWasCreated (QuizWasCreated.Parse(payload))
+    | "QuestionAddedToQuiz" -> QuestionAddedToQuiz (QuestionAddedToQuiz.Parse(payload))
+    | "QuizWasPublished" -> QuizWasPublished (QuizWasPublished.Parse(payload))
+    | e -> failwithf "Unknown event %s" e
 
 let parseEvent (event:JsonValue) =
     { 
         Id = event?id.AsString()
         Timestamp = event?timestamp.AsDateTime()
-        Payload = parsePayload event?payload (event.["type"].AsString())
+        Payload = parsePayload (event?payload.ToString()) (event.["type"].AsString())
     }
 
 let fetchEvents streamId = 
