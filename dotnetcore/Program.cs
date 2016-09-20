e6c50bdae03a10d3dc8c02d64ccb3f15a78a5dab
Michel Grootjans <michel.grootjans@protime.eu>
2016-09-20 20:52:26 +0200

    read from stream/file and project number of events

--- dotnetcore/Program.cs
+++ dotnetcore/Program.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Linq;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace projections
 {
@@ -9,18 +11,54 @@ namespace projections
     {
         public static void Main(string[] args)
         {
-            var client = new RestClient();
-
-            var projector = new Projector(client.Get("http://playing-with-projections.herokuapp.com/stream/0").Result);
+           var stream_number = 0;
+           if(args.Count() > 0) stream_number = int.Parse(args[0]);
+           var data = new RestEventReader().Read($"http://playing-with-projections.herokuapp.com/stream/{stream_number}");
+           var json = new JsonEventParser().Parse(data);
+           var projector = new Projector(json);
         }
     }
 
     public class Projector
     {
-      public Projector(JArray events)
+      public Projector(IEnumerable<Event> events)
       {
-        var numberOfEvents = events.Count();
-        Console.WriteLine("Number of events: {0}", numberOfEvents);
+        Console.WriteLine("Number of events: {0}", events.Count());
       }
     }
+
+    public class Event
+    {
+      public string id {get; set;}
+      public string type {get; set;}
+      public DateTime timestamp {get; set;}
+      public Dictionary<string, string> payload{get;set;}
+    }
+
+    public class JsonEventParser
+    {
+      public IEnumerable<Event> Parse(string data)
+      {
+          return JsonConvert.DeserializeObject<IEnumerable<Event>>(data);
+      }
+    }
+
+    public class FileEventReader
+    {
+      public string Read(string filePath)
+      {
+          Console.WriteLine($"Reading '{filePath}'");
+          return System.IO.File.ReadAllText(filePath);
+      }
+    }
+
+    public class RestEventReader
+    {
+      public string Read(string uri)
+      {
+        Console.WriteLine($"Reading '{uri}'");
+        return new RestClient().Get(uri).Result.ToString();
+      }
+    }
+
 }
