6ee8daa89dd5ffc78d27997ab3c24a2d12f00ef8
Michel Grootjans <michel.grootjans@protime.eu>
2016-09-20 09:17:53 +0200

    cleanup

--- dotnetcore/Program.cs
+++ dotnetcore/Program.cs
@@ -11,13 +11,13 @@ namespace projections
         {
             var client = new RestClient();
 
-            var projector = new Projector(client.Get("http://playing-with-projections.herokuapp.com/stream/0").Result.ToObject<List<JObject>>());
+            var projector = new Projector(client.Get("http://playing-with-projections.herokuapp.com/stream/0").Result);
         }
     }
 
     public class Projector
     {
-      public Projector(List<JObject> events)
+      public Projector(JArray events)
       {
         var numberOfEvents = events.Count();
         Console.WriteLine("Number of events: {0}", numberOfEvents);
