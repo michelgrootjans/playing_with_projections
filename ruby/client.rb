43e69e3aa3fabd382eedafc86a3de2dcf7570b4a
Michel Grootjans <michel.grootjans@protime.eu>
2016-09-21 17:34:45 +0200

    ruby cleanup

--- ruby/client.rb
+++ ruby/client.rb
@@ -2,8 +2,8 @@ require 'json'
 require 'rest-client'
 
 def read_from_uri stream_id
-  stream = "https://playing-with-projections.herokuapp.com/stream/#{stream_id}"
-#  stream = "https://raw.githubusercontent.com/tcoopman/playing_with_projections_server/master/data/#{stream_id}.json"
+  # stream = "https://playing-with-projections.herokuapp.com/stream/#{stream_id}"
+ stream = "https://raw.githubusercontent.com/tcoopman/playing_with_projections_server/master/data/#{stream_id}.json"
   puts "Reading from '#{stream}'"
   RestClient.get stream
 end
@@ -16,8 +16,8 @@ end
 
 stream_id = ARGV.first || 0
 
-json_data = read_from_uri(stream_id)
-#json_data = read_from_file(stream_id)
+raw_data = read_from_uri(stream_id)
+# raw_data = read_from_file(stream_id)
 
-hashed_response = JSON.parse(json_data)
-puts "Number of events: #{hashed_response.count}"
+events = JSON.parse(raw_data)
+puts "Number of events: #{events.count}"
