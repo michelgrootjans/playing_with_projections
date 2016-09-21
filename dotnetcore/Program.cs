a3c02acfb6a4e1444497bd2488b5a41dcb23bbb1
Michel Grootjans <michel.grootjans@protime.eu>
2016-09-21 17:46:01 +0200

    dotnet: added read from file in comment

--- dotnetcore/Program.cs
+++ dotnetcore/Program.cs
@@ -14,7 +14,8 @@ namespace projections
            var stream_id = 0;
            if(args.Count() > 0) stream_id = int.Parse(args[0]);
            var raw_data = new RestEventReader().Read($"http://playing-with-projections.herokuapp.com/stream/{stream_id}");
-          //  var raw_data = new RestEventReader().Read($"https://raw.githubusercontent.com/tcoopman/playing_with_projections_server/master/data/{stream_id}.json");
+           // var raw_data = new RestEventReader().Read($"https://raw.githubusercontent.com/tcoopman/playing_with_projections_server/master/data/{stream_id}.json");
+           // var raw_data = new FileEventReader().Read($"../data/{stream_id}.json");
            var events = new JsonEventParser().Parse(raw_data);
            var projector = new Projector(events);
 
