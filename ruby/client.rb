38d199e9261076be1b4504360a24d14d9d47e319
Michel Grootjans <michel.grootjans@protime.eu>
2016-09-22 17:04:20 +0200

    ruby client: parse date upfront

--- ruby/client.rb
+++ ruby/client.rb
@@ -2,8 +2,8 @@ require 'json'
 require 'rest-client'
 
 def read_from_uri stream_id
-  # stream = "https://playing-with-projections.herokuapp.com/stream/#{stream_id}"
-  stream = "https://raw.githubusercontent.com/tcoopman/playing_with_projections_server/master/data/#{stream_id}.json"
+  stream = "https://playing-with-projections.herokuapp.com/stream/#{stream_id}"
+  # stream = "https://raw.githubusercontent.com/tcoopman/playing_with_projections_server/master/data/#{stream_id}.json"
   puts "Reading from '#{stream}'"
   RestClient.get stream
 end
@@ -14,10 +14,15 @@ def read_from_file stream_id
   file_content = File.read(file_path)
 end
 
+def transform_date event
+  event['timestamp'] = DateTime.parse(event['timestamp'])
+  event
+end
+
 stream_id = ARGV.first || 0
 
 raw_data = read_from_uri(stream_id)
 # raw_data = read_from_file(stream_id)
 
-events = JSON.parse(raw_data)
+events = JSON.parse(raw_data).map(&method(:transform_date))
 puts "Number of events: #{events.count}"
