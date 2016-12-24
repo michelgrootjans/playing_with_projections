bb48f746019ec996b94476a3d50052ea1de670a4
Michel Grootjans <michel.grootjans@gmail.com>
2016-12-24 15:15:04 +0100

    merge

--- csharp/PlayingWithProjections/Program.cs
+++ csharp/PlayingWithProjections/Program.cs
@@ -8,25 +8,37 @@ namespace PlayingWithProjections
     {
         public static void Main(string[] args)
         {
-           var stream_id = 0;
-           if(args.Any()) stream_id = int.Parse(args[0]);
-           // var raw_data = new RestEventReader().Read($"http://playing-with-projections.herokuapp.com/stream/{stream_id}");
-           // var raw_data = new RestEventReader().Read($"https://raw.githubusercontent.com/tcoopman/playing_with_projections_server/master/data/{stream_id}.json");
-           var raw_data = new FileEventReader().Read($"../../../../data/{stream_id}.json");
-           var events = new JsonEventParser().Parse(raw_data);
-           var projector = new Projector(events);
+            var streamId = GetStreamId(args);
+            var rawData = ReadData(streamId);
+            var events = new JsonEventParser().Parse(rawData);
+            var projector = new Projector(events);
 
-           Console.WriteLine("Number of events: {0}", projector.NumberOfEvents);
+            Console.WriteLine("Number of events: {0}", projector.NumberOfEvents);
+        }
+
+        private static string ReadData(string streamId)
+        {
+            return new FileEventReader().Read($"../../../../data/{streamId}.json");
+        }
+
+        private static string GetStreamId(string[] args)
+        {
+            return args.Any() ? args[0] : "0";
         }
     }
 
     public class Projector
     {
-      readonly IEnumerable<Event> events;
-      public Projector(IEnumerable<Event> events)
-      {
-        this.events = events;
-      }
-      public int NumberOfEvents{get{ return events.Count();}}
+        readonly IEnumerable<Event> events;
+
+        public Projector(IEnumerable<Event> events)
+        {
+            this.events = events;
+        }
+
+        public int NumberOfEvents
+        {
+            get { return events.Count(); }
+        }
     }
 }
