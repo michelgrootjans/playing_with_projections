ddbd26286998e5d20cf084146ebf9fba5eae791a
Markus Krogemann <markus@krogemann.de>
2018-12-15 17:15:07 +0100

    Add a green ExUnit test to support a speedy workshop setup

---
+++ elixir/test/quizzy_client_test.exs
@@ -0,0 +1,12 @@
+defmodule QuizzyClientTest do
+  @moduledoc """
+  This is a working ExUnit test. It serves as a
+  placeholder in case you decide to write tests
+  for your solutions.
+  """
+  use ExUnit.Case
+
+  test "Elixir can add numbers" do
+    assert 42 == 40 + 2
+  end
+end
