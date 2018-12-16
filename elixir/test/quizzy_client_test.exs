ed1ce37ffcdbc56fb52226838f8c644ab35b3ca4
Markus Krogemann <markus@krogemann.de>
2018-12-16 15:13:48 +0100

    Add a very basic event counting test as a starting point
    This is to provide participants with a meaningful and working test in
    case they want to derive their solutions guided by tests.

--- elixir/test/quizzy_client_test.exs
+++ elixir/test/quizzy_client_test.exs
@@ -1,12 +1,25 @@
 defmodule QuizzyClientTest do
   @moduledoc """
   This is a working ExUnit test. It serves as a
-  placeholder in case you decide to write tests
-  for your solutions.
+  placeholder and starting point in case you decide
+  to derive your solutions guided by tests.
   """
   use ExUnit.Case
 
-  test "Elixir can add numbers" do
-    assert 42 == 40 + 2
+  @doc """
+  Please note that in this test we make use of raw
+  QuizzyClient features and also make use of a hard
+  coded file path. You may find value in extracting
+  some of these aspects into your production code
+  eventually.
+  """
+  test "that number of events in stream 0 is 4" do
+    file = File.read!("../data/0.json")
+
+    events = file
+    |> Poison.Parser.parse!(%{keys: :atoms})
+    |> Enum.map(&(QuizzyClient.parse(&1)))
+
+    assert length(events) == 4
   end
 end
